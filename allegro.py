932c55eda76e9c9b116da4767351ff0ccc2d0de7
= <=>
2022-05-12 13:53:06 -0400

    Version bump

--- allegro.py
+++ allegro.py
@@ -11,7 +11,7 @@ from whiptail import Whiptail
 from jiraconnection.jiraaccess import JiraAccess
 from timekeeping.jiratimekeeping import JiraTimekeeping
 
-VERSION="1.0.4"
+VERSION="1.0.5"
 WHIPTAIL_SETTINGS={
     "title": f"Allegro ({VERSION})",
     "width": os.get_terminal_size().columns - 10,
