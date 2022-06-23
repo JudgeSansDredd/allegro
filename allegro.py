0cc7110d701a484d88f6a294fed4b997156eb75a
= <=>
2022-06-23 15:06:59 -0400

    Update mac os build and version number

--- allegro.py
+++ allegro.py
@@ -11,7 +11,7 @@ from whiptail import Whiptail
 from jiraconnection.jiraaccess import JiraAccess
 from timekeeping.jiratimekeeping import JiraTimekeeping
 
-VERSION="1.0.7"
+VERSION="1.1.0"
 
 TERMINAL_WIDTH=os.get_terminal_size().columns
 TERMINAL_HEIGHT=os.get_terminal_size().lines
