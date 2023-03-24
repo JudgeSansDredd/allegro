7d3681e6f7f8ef9393fa9494faddecf6f738ba81
Nathan Stanley <nstanley@bandwidth.com>
2023-03-24 17:25:32 -0400

    Fix build script

--- build.sh
+++ build.sh
@@ -6,9 +6,9 @@ pip install -r requirements.txt
 pip install pyinstaller
 
 if [ "$(uname)" == "Darwin" ]; then
-  $DIST_PATH = "./dist/macos"
+  DIST_PATH="./dist/macos"
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  $DIST_PATH = "./dist/debian"
+  DIST_PATH="./dist/debian"
 fi
 
 pyinstaller allegro.py --onefile --distpath $DIST_PATH
